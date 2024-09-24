d4be709ee1631a3ad7b4e5bb051ab892a11bbce5
Joshua Fourie <joshua@fourie.se>
2024-09-24 16:55:33 +0200

    Define: defined bullets, also removed move_speed in player struct as velocity served the same purpose

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 #define SCREENWIDTH 800
 #define SCREENHEIGHT 600
+#define MAX_BULLETS 500
 
 // Struct for player
 typedef struct
 {
     Vector2 position;
     Vector2 velocity;
-    float move_speed;
     float radius;
 } Player;
+
+// Struct for bullets
+typedef struct 
+{
+    Vector2 position;
+    Vector2 velocity;
+    bool active;
+} Bullet;
 
 
 int main()
@@ -20,30 +29,32 @@ int main()
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "GAME PROJECT");
     SetTargetFPS(60);
 
-    // initializing objects 
-    Player player = { {SCREENWIDTH / 2, SCREENHEIGHT * 0.8}, {0, 0}, 200.0f, 10};
+    // Initializing player
+    Player player = { {SCREENWIDTH / 2, SCREENHEIGHT * 0.8}, {200.0f, 200.0f}, 10};
+
+    Bullet bullet [MAX_BULLETS];
 
     while(!WindowShouldClose())
     {   
         // Implementing player movement 
         if(IsKeyDown(KEY_W))
         {
-            player.position.y -= player.move_speed * GetFrameTime();
+            player.position.y -= player.velocity.y * GetFrameTime();
         }
 
         if(IsKeyDown(KEY_S))
         {
-            player.position.y += player.move_speed * GetFrameTime();
+            player.position.y += player.velocity.y * GetFrameTime();
         }
 
         if(IsKeyDown(KEY_D))
         {
-            player.position.x += player.move_speed * GetFrameTime();
+            player.position.x += player.velocity.x * GetFrameTime();
         }
 
         if(IsKeyDown(KEY_A))
         {
-            player.position.x -= player.move_speed * GetFrameTime();
+            player.position.x -= player.velocity.x * GetFrameTime();
         }
 
         BeginDrawing();
