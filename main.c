12de5f90279497e120d3f51621169591dacbd1cb
Joshua Fourie <joshua@fourie.se>
2024-09-30 11:05:45 +0200

    Implementation: implemented the function to kill enemies

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define SCREENWIDTH 800
 #define SCREENHEIGHT 600
+
 #define MAX_BULLETS 500
 #define MAX_ENEMIES 50
 
@@ -29,13 +30,18 @@ typedef struct
     Vector2 position;
     Vector2 velocity;
     bool active;
-    float width;
-    float height;
 } Enemy;
 
 
+// Initializing bullets
+Bullet bullet [MAX_BULLETS] = { 0 };
+
+// Initializing enemies
+Enemy enemy [MAX_ENEMIES] = { 0 };
+
+
 // Shoot bullets
-void shoot_bullets(Bullet bullet[], Vector2 position, Vector2 direction)
+void shoot_bullets(Vector2 position, Vector2 direction)
 {
     for(int i = 0; i < MAX_BULLETS; i++)
     {
@@ -50,7 +56,7 @@ void shoot_bullets(Bullet bullet[], Vector2 position, Vector2 direction)
 }
 
 // Update bullets
-void update_bullets(Bullet bullet[])
+void update_bullets()
 {
     for(int i = 0; i < MAX_BULLETS; i++)
     {
@@ -59,16 +65,27 @@ void update_bullets(Bullet bullet[])
             bullet[i].position.x += bullet[i].velocity.x * GetFrameTime();
             bullet[i].position.y += bullet[i].velocity.y * GetFrameTime();
 
-            if(bullet[i].position.x > SCREENWIDTH || bullet[i].position.x < 0 || bullet[i].position.y > SCREENHEIGHT || bullet[i].position.y < 0)
+            if(bullet[i].position.x > SCREENWIDTH || bullet[i].position.x < 0 || 
+            bullet[i].position.y > SCREENHEIGHT || bullet[i].position.y < 0)
             {
                 bullet[i].active = false;
+            }
+
+            for (int j = 0; j < MAX_ENEMIES; j++) {
+                if (enemy[j].active) {
+                if (CheckCollisionCircleRec(bullet[i].position, 5, (Rectangle){enemy[j].position.x, enemy[j].position.y, 32, 10})) {
+                    bullet[i].active = false;
+                    enemy[j].active = false;
+                }
+
+                }
             }
         }
     }
 }
 
 // Draw bullets 
-void draw_bullets(Bullet bullet[])
+void draw_bullets()
 {
     for(int i = 0; i < MAX_BULLETS; i++)
     {
@@ -80,7 +97,7 @@ void draw_bullets(Bullet bullet[])
 }
 
 // Spawning enemies
-void spawn_enemy(Enemy enemy[], Vector2 position, Vector2 direction)
+void spawn_enemy(Vector2 position, Vector2 direction)
 {
     for(int i = 0; i < MAX_ENEMIES; i++)
     {
@@ -89,13 +106,13 @@ void spawn_enemy(Enemy enemy[], Vector2 position, Vector2 direction)
             enemy[i].position = position;
             enemy[i].velocity = direction;
             enemy[i].active = true;
-            break;
+            return;
         }
     }
 }
 
 // Update enemies
-void update_enemy(Enemy enemy[])
+void update_enemy()
 {
     for(int i = 0; i < MAX_ENEMIES; i++)
     {
@@ -104,6 +121,7 @@ void update_enemy(Enemy enemy[])
             enemy[i].position.x += enemy[i].velocity.x * GetFrameTime();
             enemy[i].position.y += enemy[i].velocity.y * GetFrameTime();
 
+            // If the enemy goes outside the screen they will deactivate 
             if(enemy[i].position.x > SCREENWIDTH || enemy[i].position.x < 0 || 
             enemy[i].position.y > SCREENHEIGHT || enemy[i].position.y < 0)
             {
@@ -114,16 +132,17 @@ void update_enemy(Enemy enemy[])
 }
 
 // Draw enemies
-void draw_enemy(Enemy enemy[])
+void draw_enemy()
 {
    for(int i = 0; i < MAX_ENEMIES; i++)
    {
         if(enemy[i].active)
         {
-            DrawRectangleV(enemy[i].position, (Vector2) {10, 10}, WHITE);
+            DrawRectangleV(enemy[i].position, (Vector2) {32, 10}, WHITE);
         }
    } 
 }
+    
 
 int main()
 {
@@ -134,11 +153,6 @@ int main()
     // Initializing player
     Player player = {{SCREENWIDTH / 2, SCREENHEIGHT * 0.8}, {200.0f, 200.0f}, 10};
 
-    // Defining bullets 
-    Bullet bullet [MAX_BULLETS];
-
-    // Defining enemies
-    Enemy enemy [MAX_ENEMIES];
 
     float shoot_cooldown = 0.15f;
     float last_shot = 0.0f;
@@ -195,7 +209,7 @@ int main()
                 if(IsKeyDown(KEY_SPACE))
             {
                 Vector2 bulletdirection = {0, -400};
-                shoot_bullets(bullet, player.position, bulletdirection);
+                shoot_bullets(player.position, bulletdirection);
                 last_shot = 0.0f;
             }
         }
@@ -204,23 +218,23 @@ int main()
             last_shot += deltatime;
         }
         
+        // Spawning enemy logic
         int randomx = GetRandomValue(0, SCREENWIDTH);
         Vector2 enemyposition = {randomx, 0};
         Vector2 enemydirection = {0, 100};
-        spawn_enemy(enemy, enemyposition, enemydirection);
+        spawn_enemy(enemyposition, enemydirection);
         
-        update_bullets(bullet);
-
-        update_enemy(enemy);
+        update_bullets();
+        update_enemy();
 
         BeginDrawing();
         ClearBackground(BLACK);
         
         DrawCircleV(player.position, player.radius, WHITE);
 
-        draw_bullets(bullet);
+        draw_bullets();
 
-        draw_enemy(enemy);
+        draw_enemy();
 
         EndDrawing();
     }
